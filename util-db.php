e0e8ba1036f5b299f7f2e9186c722e62926de001
BrodyKelso <70341888+BrodyKelso@users.noreply.github.com>
2023-10-02 23:53:39 -0500

    Update util-db.php

--- util-db.php
+++ util-db.php
@@ -1,7 +1,7 @@
 <?php
 function get_db_connection(){
    
-    $conn = new mysqli('159.89.47.44', 'brodypke_hw3user', 'BrodyHomework', 'brodypke_hw3);
+    $conn = new mysqli('159.89.47.44', 'brodypke_hw3user', 'BrodyHomework', 'brodypke_hw3');
     
     
     if ($conn->connect_error) {
