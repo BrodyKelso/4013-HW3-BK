a11d58834f0c166b29430f3bae8878ded84df73f
BrodyKelso <70341888+BrodyKelso@users.noreply.github.com>
2023-10-02 23:55:20 -0500

    Update view-teams.php

--- view-teams.php
+++ view-teams.php
@@ -3,33 +3,27 @@
   <table class="table">
     <thead>
       <tr>
-      <th>ID</th>
-      <th>TeamName</th>
-      <th>SeasonYear</th>
-      <th>Wins</th>
-      <th>Losses</th>
+        <th>ID</th>
+        <th>TeamName</th>
+        <th>SeasonYear</th>
+        <th>Wins</th>
+        <th>Losses</th>
       </tr>
     </thead> 
     <tbody>
 <?php
 while ($team = $teams->fetch_assoc()) {
 ?>
-   <tr>
-  <td><?php echo $team ['team_id']; ?> </td>
-  <td><?php echo $team ['team_name']; ?></td>
-  <td><?php echo $team ['season_year']; ?></td>
-  <td><?php echo $team ['win']; ?></td>  
-  <td><?php echo $team ['loss']; ?></td>  
-     
-   </tr>
->?php
+      <tr>
+        <td><?php echo $team['team_id']; ?> </td>
+        <td><?php echo $team['team_name']; ?></td>
+        <td><?php echo $team['season_year']; ?></td>
+        <td><?php echo $team['win']; ?></td>  
+        <td><?php echo $team['loss']; ?></td>  
+      </tr>
+<?php
 }
 ?>
-
-
-      
     </tbody>
-
-    
   </table>
 </div>
