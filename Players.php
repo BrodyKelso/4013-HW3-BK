d20c6d24800625393480d74b4d71e2afaa5e6f92
Brody Kelso <Brody.P.Kelso-1@ou.edu>
2023-10-03 21:50:48 -0500

    ll

--- Players.php
+++ Players.php
@@ -11,11 +11,19 @@ $allPlayers = selectAllPlayers();
 
 if (isset($_GET['id']) && is_numeric($_GET['id'])) {
     $coach_id = $_GET['id'];
+    
+    // Debugging: Check the value of $coach_id
+    echo "Coach ID: " . $coach_id . "<br>";
+    
     $coach_position = getCoachPosition($coach_id);
+
+    // Debugging: Check the value of $coach_position
+    echo "Coach Position: " . $coach_position . "<br>";
 
     if ($coach_position !== null) {
         $coachedPlayers = selectPlayersByCoachPosition($coach_position);
 
+        // Debugging: Check if $coachedPlayers is not null and has rows
         if ($coachedPlayers && $coachedPlayers->num_rows > 0) {
             display_players($coachedPlayers);
         } else {
