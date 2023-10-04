b14c99dd636f5eb644824c7c6e7770704c1a7e13
BrodyKelso <70341888+BrodyKelso@users.noreply.github.com>
2023-10-03 20:09:25 -0500

    Update model-coaches-players.php

--- model-coaches-players.php
+++ model-coaches-players.php
@@ -20,4 +20,35 @@ function selectCoachesWithPlayers($coach_id){
         throw $e;
     }
 }
+
+function selectAllPlayers() {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT * FROM Players");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function selectPlayersByCoachPosition($coach_position) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT p.* FROM Players p
+                                JOIN PositionMapping pm ON pm.player_position = p.position
+                                WHERE pm.coach_position = ?");
+        $stmt->bind_param("s", $coach_position);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
 ?>
