343299c5a6f991b215156e4666b5f067c9aaaf1a
Brody Kelso <Brody.P.Kelso-1@ou.edu>
2023-10-04 00:08:43 -0500

    ss

--- Players.php
+++ Players.php
@@ -9,6 +9,9 @@ include "view-header.php";
 $positionFilter = isset($_POST['filterPosition']) ? $_POST['filterPosition'] : null;
 $allPlayers = selectAllPlayers($positionFilter); // Modify selectAllPlayers() to accept filter
 
+// Add this line for debugging
+var_dump($allPlayers); 
+
 include "view-players.php";
 include "view-footer.php";
 ?>
