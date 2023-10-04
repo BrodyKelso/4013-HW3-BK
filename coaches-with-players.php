e512d7fc12668c0cb0ea19cc5147a5d15bc2f674
BrodyKelso <70341888+BrodyKelso@users.noreply.github.com>
2023-10-03 20:15:23 -0500

    Update coaches-with-players.php

--- coaches-with-players.php
+++ coaches-with-players.php
@@ -5,20 +5,18 @@ require_once("model-coaches-players.php");
 $pageTitle = "Coaches with Players";
 include "view-header.php";
 
-$coach_id = $_GET['id'] ?? null;
-$coach_position = $_GET['position'] ?? null;
+// Ensure that an ID is provided and is a number
+if(isset($_GET['id']) && is_numeric($_GET['id'])) {
+    $coachesPlayers = selectCoachesWithPlayers($_GET['id']);
 
-if($coach_position === 'Head Coach') {
-    $players = selectAllPlayers();
-} elseif($coach_position !== null) {
-    $players = selectPlayersByCoachPosition($coach_position);
-} elseif($coach_id !== null) {
-    $players = selectCoachesWithPlayers($coach_id);
+    // Check if data was fetched
+    if($coachesPlayers && $coachesPlayers->num_rows > 0) {
+        include "view-players.php";
+    } else {
+        echo "<p>No players found for this coach.</p>";
+    }
 } else {
-    // Handle error: neither id nor position supplied.
-    die("Error: No ID or Position supplied.");
+    echo "<p>Invalid coach ID.</p>";
 }
 
-include "view-players.php";
 include "view-footer.php";
-?>
