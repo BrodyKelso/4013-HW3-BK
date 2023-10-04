2017d6f99fed9d6d5b4b6f4f4a581e88a1edb7bb
Brody Kelso <Brody.P.Kelso-1@ou.edu>
2023-10-04 00:24:12 -0500

    ss

--- Players.php
+++ Players.php
@@ -6,10 +6,14 @@ require_once("view-players.php");
 $pageTitle = "Players";
 include "view-header.php";
 
-// Handle the filter here
 $positionFilter = isset($_POST['filterPosition']) ? $_POST['filterPosition'] : null;
-$allPlayers = selectAllPlayers1($positionFilter);
+$allPlayers = selectAllPlayers($positionFilter); // Modify selectAllPlayers() to accept filter
 
-include "view-players.php";
+if ($allPlayers !== null && $allPlayers->num_rows > 0) {
+    include "view-players.php";
+} else {
+    echo "No player data available."; // Display a message when there's no data.
+}
+
 include "view-footer.php";
 ?>
