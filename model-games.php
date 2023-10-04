8b059f7460f8fdbfe7cb9a41e5aa2c2c39b42934
BrodyKelso <70341888+BrodyKelso@users.noreply.github.com>
2023-10-03 20:48:56 -0500

    Update model-games.php

--- model-games.php
+++ model-games.php
@@ -4,7 +4,7 @@ require_once("util-db.php");
 function selectAllGames() {
     global $dbConnection; // assuming you have a $dbConnection in util-db.php
 
-    $query = "SELECT * FROM games"; // adjust this SQL according to your table and fields
+    $query = "SELECT * FROM Games"; // adjust this SQL according to your table and fields
     $result = $dbConnection->query($query);
 
     return $result;
