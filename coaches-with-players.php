a3b52c9dca3e1c577a5c8925adb379f5609a7d5f
Brody Kelso <Brody.P.Kelso-1@ou.edu>
2023-10-03 22:20:42 -0500

    kk

--- coaches-with-players.php
+++ coaches-with-players.php
@@ -1,6 +1,6 @@
 <?php
 require_once("util-db.php");
-require_once("model-coaches-players.php"); // Include the coaches-players functions
+require_once("model-coaches-players.php");
 
 $pageTitle = "Coaches with Players";
 include "view-header.php";
@@ -8,14 +8,15 @@ include "view-header.php";
 // Ensure that an ID is provided and is a number
 if(isset($_GET['id']) && is_numeric($_GET['id'])) {
     $coach_id = $_GET['id'];
+    
+    // Get the coach's position
     $coach_position = getCoachPosition($coach_id);
 
     if ($coach_position !== null) {
         $coachedPlayers = selectPlayersByCoachPosition($coach_position);
 
-        // Check if data was fetched
         if ($coachedPlayers && $coachedPlayers->num_rows > 0) {
-            include "view-players.php";
+            include "view-coaches-players.php"; // Include the view for displaying players
         } else {
             echo "<p>No players found for this coach.</p>";
         }
