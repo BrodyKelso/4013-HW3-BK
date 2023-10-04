3e592d06c0dc9ce746c499511e42f5f4df307490
Brody Kelso <Brody.P.Kelso-1@ou.edu>
2023-10-03 22:49:27 -0500

    ddd

--- coaches-with-players.php
+++ coaches-with-players.php
@@ -8,13 +8,10 @@ include "view-header.php";
 // Ensure that an ID is provided and is a number
 if(isset($_GET['id']) && is_numeric($_GET['id'])) {
     $coach_id = $_GET['id'];
-    
-    // Debugging: Check the value of $coach_id
-    echo "Coach ID: " . $coach_id . "<br>";
 
     $coachesPlayers = selectCoachesWithPlayers($coach_id);
 
-    // Debugging: Check if $coachesPlayers is null or has rows
+    // Check if $coachesPlayers is null or has rows
     if ($coachesPlayers === null) {
         echo "Coaches Players result is null!";
     } elseif ($coachesPlayers->num_rows === 0) {
@@ -22,9 +19,4 @@ if(isset($_GET['id']) && is_numeric($_GET['id'])) {
     } else {
         include "view-players.php";
     }
-} else {
-    echo "<p>Invalid coach ID.</p>";
-}
-
-include "view-footer.php";
-?>
+} else
