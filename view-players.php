2b7ed009069b3173800a34271e8710f0ef364b6c
Brody Kelso <Brody.P.Kelso-1@ou.edu>
2023-10-04 00:16:43 -0500

    ss

--- view-players.php
+++ view-players.php
@@ -1,9 +1,3 @@
-<?php
-if (!isset($allPlayers)) {
-    die("Player data is not available.");
-}
-?>
-
 <h1>Players</h1>
 <form method="POST" action="Players.php" class="mb-3">
     <label for="filterPosition" class="form-label">Filter by Position</label>
@@ -15,39 +9,50 @@ if (!isset($allPlayers)) {
         <!-- Add other positions as per your data -->
     </select>
 </form>
-<div class="table-responsive">
-    <table class="table">
-        <thead>
-            <tr>
-                <th>ID</th>
-                <th>First Name</th>
-                <th>Last Name</th>
-                <th>Position</th>
-                <th>Jersey Number</th>
-                <th>Year</th>
-                <th>Team ID</th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php
-            while ($player = $playersData->fetch_assoc()) {
-                // Check if filterPosition is set and if the player's position matches the filter
-                if (isset($_POST['filterPosition']) && $_POST['filterPosition'] !== "" && $player['position'] !== $_POST['filterPosition']) {
-                    continue; // Skip this player if it doesn't match the filter
+
+<?php
+if ($playersData->num_rows > 0) {
+    // Display the table only if there are players to show
+    ?>
+    <div class="table-responsive">
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>First Name</th>
+                    <th>Last Name</th>
+                    <th>Position</th>
+                    <th>Jersey Number</th>
+                    <th>Year</th>
+                    <th>Team ID</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                while ($player = $playersData->fetch_assoc()) {
+                    // Check if filterPosition is set and if the player's position matches the filter
+                    if (isset($_POST['filterPosition']) && $_POST['filterPosition'] !== "" && $player['position'] !== $_POST['filterPosition']) {
+                        continue; // Skip this player if it doesn't match the filter
+                    }
+                    ?>
+                    <tr>
+                        <td><?php echo $player['player_id']; ?></td>
+                        <td><?php echo $player['first_name']; ?></td>
+                        <td><?php echo $player['last_name']; ?></td>
+                        <td><?php echo $player['position']; ?></td>
+                        <td><?php echo $player['jersey_number']; ?></td>
+                        <td><?php echo $player['year']; ?></td>
+                        <td><?php echo $player['team_id']; ?></td>
+                    </tr>
+                    <?php
                 }
                 ?>
-                <tr>
-                    <td><?php echo $player['player_id']; ?></td>
-                    <td><?php echo $player['first_name']; ?></td>
-                    <td><?php echo $player['last_name']; ?></td>
-                    <td><?php echo $player['position']; ?></td>
-                    <td><?php echo $player['jersey_number']; ?></td>
-                    <td><?php echo $player['year']; ?></td>
-                    <td><?php echo $player['team_id']; ?></td>
-                </tr>
-                <?php
-            }
-            ?>
-        </tbody>
-    </table>
-</div>
+            </tbody>
+        </table>
+    </div>
+    <?php
+} else {
+    // No players to display
+    echo "No player data available.";
+}
+?>
