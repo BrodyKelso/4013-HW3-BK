0976db7449052653cbf77dddd193bdd39bb5d074
BrodyKelso <70341888+BrodyKelso@users.noreply.github.com>
2023-10-03 22:55:04 -0500

    Create view-coaches-players-players.php

---
+++ view-coaches-players-players.php
@@ -0,0 +1,37 @@
+<?php
+// Assuming you have the function display_players defined in this file
+// or you can include the necessary functions here.
+
+// Display a table of players
+?>
+<h1>Players Coached</h1>
+<div class="table-responsive">
+  <table class="table">
+    <thead>
+      <tr>
+        <th>ID</th>
+        <th>First Name</th>
+        <th>Last Name</th>
+        <th>Position</th>
+        <th>Jersey Number</th>
+        <th>Year</th>
+      </tr>
+    </thead> 
+    <tbody>
+      <?php
+      while ($player = $coachesPlayers->fetch_assoc()) {
+      ?>
+        <tr>
+          <td><?php echo $player['player_id']; ?></td>
+          <td><?php echo $player['first_name']; ?></td>
+          <td><?php echo $player['last_name']; ?></td>
+          <td><?php echo $player['position']; ?></td>
+          <td><?php echo $player['jersey_number']; ?></td>
+          <td><?php echo $player['year']; ?></td>
+        </tr>
+      <?php
+      }
+      ?>
+    </tbody>
+  </table>
+</div>
