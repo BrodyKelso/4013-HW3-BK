0ff2fe3d1ef830e956610a6ea508192d2abe3369
BrodyKelso <70341888+BrodyKelso@users.noreply.github.com>
2023-10-03 20:46:48 -0500

    Create model-games.php

---
+++ model-games.php
@@ -0,0 +1,12 @@
+<?php
+require_once("util-db.php");
+
+function selectAllGames() {
+    global $dbConnection; // assuming you have a $dbConnection in util-db.php
+
+    $query = "SELECT * FROM games"; // adjust this SQL according to your table and fields
+    $result = $dbConnection->query($query);
+
+    return $result;
+}
+?>
