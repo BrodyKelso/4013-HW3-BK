c6aa62f5205cec37a7cfff1ee9c214b7a5a7b8da
BrodyKelso <70341888+BrodyKelso@users.noreply.github.com>
2023-10-03 20:36:30 -0500

    Update Players.php

--- Players.php
+++ Players.php
@@ -4,7 +4,15 @@ require_once("model-players.php");
 
 $pageTitle = "Players";
 include "view-header.php";
-$players = selectAllPlayers();
-include "view-players.php";
+
+$allPlayers = selectAllPlayers();
+
+// Check if data was fetched
+if($allPlayers && $allPlayers->num_rows > 0) {
+    include "view-players.php";
+} else {
+    echo "<p>No players found.</p>";
+}
+
 include "view-footer.php";
 ?>
