c761ece6de07e44bb864068b36dde0e3f8e5b91a
Brody Kelso <Brody.P.Kelso-1@ou.edu>
2023-10-03 22:40:17 -0500

    kfk

--- coaches-with-players.php
+++ coaches-with-players.php
@@ -1,3 +1,4 @@
+<?php
 require_once("util-db.php");
 require_once("model-coaches-players.php");
 
@@ -8,16 +9,14 @@ include "view-header.php";
 if(isset($_GET['id']) && is_numeric($_GET['id'])) {
     $coachesPlayers = selectCoachesWithPlayers($_GET['id']);
 
-    // Debugging: Check if $coachesPlayers is null or has rows
-    if ($coachesPlayers === null) {
-        echo "Coaches Players result is null!";
-    } elseif ($coachesPlayers->num_rows === 0) {
-        echo "No rows found in Coaches Players result!";
-    } else {
+    if ($coachesPlayers && $coachesPlayers->num_rows > 0) {
         include "view-players.php";
+    } else {
+        echo "<p>No players found for this coach.</p>";
     }
 } else {
     echo "<p>Invalid coach ID.</p>";
 }
 
 include "view-footer.php";
+?>
