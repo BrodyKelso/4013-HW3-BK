1bb972686e0eacadebe2b4f3eda10ee619016c76
Brody Kelso <Brody.P.Kelso-1@ou.edu>
2023-10-04 00:11:44 -0500

    ss

--- Players.php
+++ Players.php
@@ -7,10 +7,19 @@ $pageTitle = "Players";
 include "view-header.php";
 
 $positionFilter = isset($_POST['filterPosition']) ? $_POST['filterPosition'] : null;
+
+// Debugging: Output the value of $positionFilter
+echo "Filter position: " . $positionFilter;
+
 $allPlayers = selectAllPlayers($positionFilter); // Modify selectAllPlayers() to accept filter
 
-// Add this line for debugging
-var_dump($allPlayers); 
+if ($allPlayers) {
+    // Debugging: Output the count of retrieved players
+    echo "Number of players retrieved: " . $allPlayers->num_rows;
+} else {
+    // Debugging: Output an error message if $allPlayers is not populated
+    echo "Player data is not available.";
+}
 
 include "view-players.php";
 include "view-footer.php";
