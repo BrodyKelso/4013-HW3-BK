6eb719dec8b2eb71c61a7b8eb0d7e6f1937a388c
BrodyKelso <70341888+BrodyKelso@users.noreply.github.com>
2023-10-03 19:48:12 -0500

    Update model-coaches-players.php

--- model-coaches-players.php
+++ model-coaches-players.php
@@ -1,16 +1,21 @@
 <?php
-function selectCoachesWithPlayers() {
+function selectCoachesWithPlayers($coach_id) {
     try {
         $conn = get_db_connection();
         $stmt = $conn->prepare("
             SELECT Coaches.first_name AS coach_first_name, 
                    Coaches.last_name AS coach_last_name, 
                    Players.first_name AS player_first_name, 
-                   Players.last_name AS player_last_name 
+                   Players.last_name AS player_last_name, 
+                   Players.position AS player_position,
+                   Players.jersey_number AS jersey_number,
+                   Players.year AS player_year
             FROM Coaches
-            JOIN Teams ON Coaches.team_id = Teams.team_id
-            JOIN Players ON Teams.team_id = Players.team_id
+            JOIN Players ON Coaches.coach_id = Players.team_id
+            WHERE Coaches.coach_id = ?
+            ORDER BY Players.player_id
         ");
+        $stmt->bind_param("i", $coach_id);
         $stmt->execute();
         $result = $stmt->get_result();
         $conn->close();
