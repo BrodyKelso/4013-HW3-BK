e14c321ca3f57d9fe6a0c2a8ae2da025385b9cd9
BrodyKelso <70341888+BrodyKelso@users.noreply.github.com>
2023-10-03 20:09:39 -0500

    Update coaches-with-players.php

--- coaches-with-players.php
+++ coaches-with-players.php
@@ -4,7 +4,21 @@ require_once("model-coaches-players.php");
 
 $pageTitle = "Coaches with Players";
 include "view-header.php";
-$coachesPlayers = selectCoachesWithPlayers($_GET['id']);
-include "view-coaches-players.php";
+
+$coach_id = $_GET['id'] ?? null;
+$coach_position = $_GET['position'] ?? null;
+
+if($coach_position === 'Head Coach') {
+    $players = selectAllPlayers();
+} elseif($coach_position !== null) {
+    $players = selectPlayersByCoachPosition($coach_position);
+} elseif($coach_id !== null) {
+    $players = selectCoachesWithPlayers($coach_id);
+} else {
+    // Handle error: neither id nor position supplied.
+    die("Error: No ID or Position supplied.");
+}
+
+include "view-players.php";
 include "view-footer.php";
 ?>
