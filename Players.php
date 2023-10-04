078a7641623678a90bc0c55370d74fdffb24449e
Brody Kelso <Brody.P.Kelso-1@ou.edu>
2023-10-04 00:15:29 -0500

    ss

--- Players.php
+++ Players.php
@@ -6,20 +6,9 @@ require_once("view-players.php");
 $pageTitle = "Players";
 include "view-header.php";
 
+// Handle the filter here
 $positionFilter = isset($_POST['filterPosition']) ? $_POST['filterPosition'] : null;
-
-// Debugging: Output the value of $positionFilter
-echo "Filter position: " . $positionFilter;
-
-$allPlayers = selectAllPlayers($positionFilter); // Modify selectAllPlayers() to accept filter
-
-if ($allPlayers) {
-    // Debugging: Output the count of retrieved players
-    echo "Number of players retrieved: " . $allPlayers->num_rows;
-} else {
-    // Debugging: Output an error message if $allPlayers is not populated
-    echo "Player data is not available.";
-}
+$allPlayers = selectAllPlayers($positionFilter);
 
 include "view-players.php";
 include "view-footer.php";
--- view-players.php
+++ view-players.php
@@ -4,7 +4,6 @@ if (!isset($allPlayers)) {
 }
 ?>
 
-
 <h1>Players</h1>
 <form method="POST" action="Players.php" class="mb-3">
     <label for="filterPosition" class="form-label">Filter by Position</label>
@@ -17,38 +16,38 @@ if (!isset($allPlayers)) {
     </select>
 </form>
 <div class="table-responsive">
-  <table class="table">
-    <thead>
-      <tr>
-        <th>ID</th>
-        <th>First Name</th>
-        <th>Last Name</th>
-        <th>Position</th>
-        <th>Jersey Number</th>
-        <th>Year</th>
-        <th>Team ID</th>
-      </tr>
-    </thead> 
-    <tbody>
-<?php
-while ($player = $playersData->fetch_assoc()) {
-    // Check if filterPosition is set and if the player's position matches the filter
-    if (isset($_POST['filterPosition']) && $_POST['filterPosition'] !== "" && $player['position'] !== $_POST['filterPosition']) {
-        continue; // Skip this player if it doesn't match the filter
-    }
-?>
-      <tr>
-        <td><?php echo $player['player_id']; ?></td>
-        <td><?php echo $player['first_name']; ?></td>
-        <td><?php echo $player['last_name']; ?></td>
-        <td><?php echo $player['position']; ?></td>
-        <td><?php echo $player['jersey_number']; ?></td>  
-        <td><?php echo $player['year']; ?></td>  
-        <td><?php echo $player['team_id']; ?></td>  
-      </tr>
-<?php
-}
-?>
-    </tbody>
-  </table>
+    <table class="table">
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>First Name</th>
+                <th>Last Name</th>
+                <th>Position</th>
+                <th>Jersey Number</th>
+                <th>Year</th>
+                <th>Team ID</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+            while ($player = $playersData->fetch_assoc()) {
+                // Check if filterPosition is set and if the player's position matches the filter
+                if (isset($_POST['filterPosition']) && $_POST['filterPosition'] !== "" && $player['position'] !== $_POST['filterPosition']) {
+                    continue; // Skip this player if it doesn't match the filter
+                }
+                ?>
+                <tr>
+                    <td><?php echo $player['player_id']; ?></td>
+                    <td><?php echo $player['first_name']; ?></td>
+                    <td><?php echo $player['last_name']; ?></td>
+                    <td><?php echo $player['position']; ?></td>
+                    <td><?php echo $player['jersey_number']; ?></td>
+                    <td><?php echo $player['year']; ?></td>
+                    <td><?php echo $player['team_id']; ?></td>
+                </tr>
+                <?php
+            }
+            ?>
+        </tbody>
+    </table>
 </div>
