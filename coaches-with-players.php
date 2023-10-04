7fac2266956b05342b78c0221aa64e189302f669
Brody Kelso <Brody.P.Kelso-1@ou.edu>
2023-10-03 22:37:17 -0500

    ll

--- coaches-with-players.php
+++ coaches-with-players.php
@@ -1,4 +1,3 @@
-<?php
 require_once("util-db.php");
 require_once("model-coaches-players.php");
 
@@ -7,25 +6,18 @@ include "view-header.php";
 
 // Ensure that an ID is provided and is a number
 if(isset($_GET['id']) && is_numeric($_GET['id'])) {
-    $coach_id = $_GET['id'];
-    
-    // Get the coach's position
-    $coach_position = getCoachPosition($coach_id);
+    $coachesPlayers = selectCoachesWithPlayers($_GET['id']);
 
-    if ($coach_position !== null) {
-        $coachedPlayers = selectPlayersByCoachPosition($coach_position);
-
-        if ($coachedPlayers && $coachedPlayers->num_rows > 0) {
-            include "view-coaches-players.php"; // Include the view for displaying players
-        } else {
-            echo "<p>No players found for this coach.</p>";
-        }
+    // Debugging: Check if $coachesPlayers is null or has rows
+    if ($coachesPlayers === null) {
+        echo "Coaches Players result is null!";
+    } elseif ($coachesPlayers->num_rows === 0) {
+        echo "No rows found in Coaches Players result!";
     } else {
-        echo "<p>Invalid coach ID or coach position not specified.</p>";
+        include "view-players.php";
     }
 } else {
     echo "<p>Invalid coach ID.</p>";
 }
 
 include "view-footer.php";
-?>
