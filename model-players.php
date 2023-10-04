e18220a483ab8afc2b5dc29ade3d3e388f0ba982
Brody Kelso <Brody.P.Kelso-1@ou.edu>
2023-10-03 23:22:08 -0500

    dd

--- model-players.php
+++ model-players.php
@@ -4,7 +4,7 @@ require_once("util-db.php");
 function selectAllPlayers($positionFilter = null) {
     global $conn; 
     
-    $sql = "SELECT * FROM Players"; 
+    $sql = "SELECT player_id, first_name, last_name, position, jersey_number, year FROM Players"; 
     
     if($positionFilter) {
         $positionFilter = mysqli_real_escape_string($conn, $positionFilter); 
