f76a5ad6ca731b53b595f6f60ac5cf2e77b4c96e
Brody Kelso <Brody.P.Kelso-1@ou.edu>
2023-10-03 22:13:08 -0500

    ghlh

--- coaches-with-players.php
+++ coaches-with-players.php
@@ -8,13 +8,19 @@ include "view-header.php";
 // Ensure that an ID is provided and is a number
 if(isset($_GET['id']) && is_numeric($_GET['id'])) {
     $coach_id = $_GET['id'];
-    $coachesPlayers = selectCoachesWithPlayers($coach_id);
+    $coach_position = getCoachPosition($coach_id);
 
-    // Check if data was fetched
-    if($coachesPlayers && $coachesPlayers->num_rows > 0) {
-        include "view-players.php";
+    if ($coach_position !== null) {
+        $coachedPlayers = selectPlayersByCoachPosition($coach_position);
+
+        // Check if data was fetched
+        if ($coachedPlayers && $coachedPlayers->num_rows > 0) {
+            include "view-players.php";
+        } else {
+            echo "<p>No players found for this coach.</p>";
+        }
     } else {
-        echo "<p>No players found for this coach.</p>";
+        echo "<p>Invalid coach ID or coach position not specified.</p>";
     }
 } else {
     echo "<p>Invalid coach ID.</p>";
