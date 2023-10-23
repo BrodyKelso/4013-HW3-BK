5a9e0933e76c6c9a691c289c905d1040f98ecd4f
Brody Kelso <Brody.P.Kelso-1@ou.edu>
2023-10-23 18:38:32 -0500

    more

--- model-teams.php
+++ model-teams.php
@@ -13,14 +13,42 @@ function selectTeams() {
     }
 }
 
-function insertTeams() {
+function insertTeams($team_name, $season_year, $win, $loss) {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT team_id, team_name,season_year, win, loss FROM Teams");
-        $stmt->execute();
-        $result = $stmt->get_result();
+        $stmt = $conn->prepare("INSERT INTO `Teams` (`team_name`, `season_year`, `win`, `loss`) VALUES ('?', '?', '?', '?');");
+        $stmt->bind_param("ssss", $team_name, $season_year, $win, $loss);
+        $success = $stmt->execute();
         $conn->close();
-        return $result;
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function updateTeams($team_name, $season_year, $win, $loss, $team_id) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("UPDATE `Teams` set (`team_name` = ?, `season_year` = ?, `win` = ?, `loss` = ?) WHERE `team_id` = ?;");
+        $stmt->bind_param("ssssi", $team_name, $season_year, $win, $loss, $team_id);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function deleteTeams($team_id) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("DELETE FROM `Teams` WHERE `team_id` = ?;");
+        $stmt->bind_param("i", $team_id);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
     } catch (Exception $e) {
         $conn->close();
         throw $e;
