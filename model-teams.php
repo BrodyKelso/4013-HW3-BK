a92bbdddc7597f82f5fea3527b05f5991443ee4c
Brody Kelso <Brody.P.Kelso-1@ou.edu>
2023-10-23 16:06:23 -0500

    v

--- model-teams.php
+++ model-teams.php
@@ -13,4 +13,19 @@ function selectTeams() {
     }
 }
 
+function insertTeams() {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT team_id, team_name,season_year, win, loss FROM Teams");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+
 ?>
