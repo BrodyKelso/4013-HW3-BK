1a024b361676e870aa180dfe7bbf419e352f1a40
Brody Kelso <Brody.P.Kelso-1@ou.edu>
2023-10-23 23:06:25 -0500

    ;;

--- view-coaches.php
+++ view-coaches.php
@@ -23,32 +23,30 @@
             </tr>
         </thead>
         <tbody>
-            <?php
-            while ($coach = $coaches->fetch_assoc()) {
-            ?>
-                <tr>
-                    <td><?php echo $coach['coach_id']; ?></td>
-                    <td><?php echo $coach['first_name']; ?></td>
-                    <td><?php echo $coach['last_name']; ?></td>
-                    <td><?php echo $coach['position']; ?></td>
-                    <td><?php echo $coach['team_id']; ?></td>
-                    
-                    <td>
-                        <?php include "view-coaches-editform.php"; ?>
-                    </td>
-                    
-                    <td>
-                        <!-- Delete action -->
-                        <form method="post">
-                            <input type="hidden" name="actionType" value="Delete">
-                            <input type="hidden" name="coach_id" value="<?php echo $coach['coach_id']; ?>">
-                            <button type="submit" class="btn btn-danger">Delete</button>
-                        </form>
-                    </td>
-                </tr>
-            <?php
-            }
-            ?>
-        </tbody>
+    <?php
+    while ($coach = $coaches->fetch_assoc()) {
+    ?>
+        <tr>
+            <td><?php echo isset($coach['coach_id']) ? $coach['coach_id'] : 'N/A'; ?></td>
+            <td><?php echo isset($coach['first_name']) ? $coach['first_name'] : 'N/A'; ?></td>
+            <td><?php echo isset($coach['last_name']) ? $coach['last_name'] : 'N/A'; ?></td>
+            <td><?php echo isset($coach['team_id']) ? $coach['team_id'] : 'N/A'; ?></td> <!-- Here's the line -->
+            <td>
+                <?php include "view-coaches-editform.php"; ?>
+            </td>
+            <td>
+                <!-- Delete action -->
+                <form method="post">
+                    <input type="hidden" name="actionType" value="Delete">
+                    <input type="hidden" name="coach_id" value="<?php echo isset($coach['coach_id']) ? $coach['coach_id'] : ''; ?>">
+                    <button type="submit" class="btn btn-danger">Delete</button>
+                </form>
+            </td>
+        </tr>
+    <?php
+    }
+    ?>
+</tbody>
+
     </table>
 </div>
