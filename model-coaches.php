f53e808a8ecb80b87f73452111748e1591a75034
Brody Kelso <Brody.P.Kelso-1@ou.edu>
2023-10-23 23:26:56 -0500

    dd

--- model-coaches.php
+++ model-coaches.php
@@ -16,7 +16,7 @@ function selectCoaches() {
 function insertCoach($first_name, $last_name, $position, $team_id) {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("INSERT INTO `Coaches` (first_name, last_name, position, team_id) VALUES (?, ?, ?, ?)");
+        $stmt = $conn->prepare("INSERT INTO Coaches (first_name, last_name, position, team_id) VALUES (?, ?, ?, ?)");
         $stmt->bind_param("sssi", $first_name, $last_name, $position, $team_id);
         $success = $stmt->execute();
         $conn->close();
@@ -30,7 +30,7 @@ function insertCoach($first_name, $last_name, $position, $team_id) {
 function updateCoach($coach_id, $first_name, $last_name, $position, $team_id) {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("UPDATE `Coaches` SET first_name = ?, last_name = ?, position = ?, team_id = ? WHERE coach_id = ?");
+        $stmt = $conn->prepare("UPDATE Coaches SET first_name = ?, last_name = ?, position = ?, team_id = ? WHERE coach_id = ?");
         $stmt->bind_param("sssii", $first_name, $last_name, $position, $team_id, $coach_id);
         $success = $stmt->execute();
         $conn->close();
@@ -44,7 +44,7 @@ function updateCoach($coach_id, $first_name, $last_name, $position, $team_id) {
 function deleteCoach($coach_id) {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("DELETE FROM `Coaches` WHERE coach_id = ?");
+        $stmt = $conn->prepare("DELETE FROM Coaches WHERE coach_id = ?");
         $stmt->bind_param("i", $coach_id);
         $success = $stmt->execute();
         $conn->close();
@@ -54,5 +54,4 @@ function deleteCoach($coach_id) {
         throw $e;
     }
 }
-
 ?>
