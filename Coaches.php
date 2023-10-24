4ec675b7fb520f060d773afeee306c7efab1b749
Brody Kelso <Brody.P.Kelso-1@ou.edu>
2023-10-23 23:50:14 -0500

    ll

--- Coaches.php
+++ Coaches.php
@@ -5,42 +5,52 @@ require_once("model-coaches.php");
 $pageTitle = "Coaches";
 include "view-header.php";
 
-if (isset($_POST['actionType']))
-{
-    switch ($_POST['actionType'])
-    {
-        case "Add":
-            if (insertCoach($_POST['first_name'], $_POST['last_name'], $_POST['position'], $_POST['team_id']))
-            {
-                echo '<div class="alert alert-success" role="alert">Coach added.</div>';
-            }
-            else
-            {
-                echo '<div class="alert alert-danger" role="alert">Coach not added.</div>';
-            }
-            break;
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    if (isset($_POST['actionType'])) {
+        switch ($_POST['actionType']) {
+            case "Add":
+                if (isset($_POST['first_name'], $_POST['last_name'], $_POST['position'], $_POST['team_id'])) {
+                    $first_name = $_POST['first_name'];
+                    $last_name = $_POST['last_name'];
+                    $position = $_POST['position'];
+                    $team_id = $_POST['team_id'];
 
-        case "Edit":
-            if (updateCoach($_POST['coach_id'], $_POST['first_name'], $_POST['last_name'], $_POST['position'], $_POST['team_id']))
-            {
-                echo '<div class="alert alert-success" role="alert">Coach updated.</div>';
-            }
-            else
-            {
-                echo '<div class="alert alert-danger" role="alert">Coach not updated.</div>';
-            }
-            break;
+                    if (insertCoach($first_name, $last_name, $position, $team_id)) {
+                        echo '<div class="alert alert-success" role="alert">Coach added.</div>';
+                    } else {
+                        echo '<div class="alert alert-danger" role="alert">Coach not added.</div>';
+                    }
+                }
+                break;
 
-        case "Delete":
-            if (deleteCoach($_POST['coach_id']))
-            {
-                echo '<div class="alert alert-success" role="alert">Coach deleted.</div>';
-            }
-            else
-            {
-                echo '<div class="alert alert-danger" role="alert">Coach not deleted.</div>';
-            }
-            break;
+            case "Edit":
+                if (isset($_POST['coach_id'], $_POST['first_name'], $_POST['last_name'], $_POST['position'], $_POST['team_id'])) {
+                    $coach_id = $_POST['coach_id'];
+                    $first_name = $_POST['first_name'];
+                    $last_name = $_POST['last_name'];
+                    $position = $_POST['position'];
+                    $team_id = $_POST['team_id'];
+
+                    if (updateCoach($coach_id, $first_name, $last_name, $position, $team_id)) {
+                        echo '<div class="alert alert-success" role="alert">Coach updated.</div>';
+                    } else {
+                        echo '<div class="alert alert-danger" role="alert">Coach not updated.</div>';
+                    }
+                }
+                break;
+
+            case "Delete":
+                if (isset($_POST['coach_id'])) {
+                    $coach_id = $_POST['coach_id'];
+
+                    if (deleteCoach($coach_id)) {
+                        echo '<div class="alert alert-success" role="alert">Coach deleted.</div>';
+                    } else {
+                        echo '<div class="alert alert-danger" role="alert">Coach not deleted.</div>';
+                    }
+                }
+                break;
+        }
     }
 }
 
