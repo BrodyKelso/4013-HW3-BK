ebc75f254b9f9838ca4398e8d3eddd57179ff160
BrodyKelso <Brody.P.Kelso-1@ou.edu>
2023-10-24 22:50:11 -0500

    dd

--- NewPlayers.php
+++ NewPlayers.php
@@ -1,13 +1,13 @@
 <?php
 require_once("util-db.php");
 require_once("model-players.php");
-require_once("view-players.php");
+require_once("view-newplayers.php");
 
 $pageTitle = "Players";
 include "view-header.php";
 
 
-$allPlayers = selectAllPlayers1($positionFilter); // Modify selectAllPlayers() to accept filter
+$allPlayers = selectAllPlayers1(); // Modify selectAllPlayers() to accept filter
 
 include "view-players.php";
 include "view-footer.php";
---
+++ view-newplayers.php
@@ -0,0 +1,58 @@
+<h1>Players</h1>
+<form method="POST" action="Players.php" class="mb-3">
+    <label for="filterPosition" class="form-label">Filter by Position</label>
+    <select name="filterPosition" id="filterPosition" class="form-select" onchange="this.form.submit()">
+        <option value="">--Select Position--</option>
+        <option value="QB">Quarterback</option>
+        <option value="RB">Running Back</option>
+        <option value="WR">Wide Receiver</option>
+        <!-- Add other positions as per your data -->
+    </select>
+</form>
+
+<?php
+if (isset($allPlayers) && $allPlayers->num_rows > 0) {
+    // Display the table only if there are players to show
+    ?>
+    <div class="table-responsive">
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>First Name</th>
+                    <th>Last Name</th>
+                    <th>Position</th>
+                    <th>Jersey Number</th>
+                    <th>Year</th>
+                    <!-- Removed "Team ID" header -->
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                while ($player = $allPlayers->fetch_assoc()) {
+                    // Check if filterPosition is set and if the player's position matches the filter
+                    if (isset($_POST['filterPosition']) && $_POST['filterPosition'] !== "" && $player['position'] !== $_POST['filterPosition']) {
+                        continue; // Skip this player if it doesn't match the filter
+                    }
+                    ?>
+                    <tr>
+                        <td><?php echo $player['player_id']; ?></td>
+                        <td><?php echo $player['first_name']; ?></td>
+                        <td><?php echo $player['last_name']; ?></td>
+                        <td><?php echo $player['position']; ?></td>
+                        <td><?php echo $player['jersey_number']; ?></td>
+                        <td><?php echo $player['year']; ?></td>
+                        <!-- Removed displaying "Team ID" -->
+                    </tr>
+                    <?php
+                }
+                ?>
+            </tbody>
+        </table>
+    </div>
+    <?php
+} else {
+    // No players to display
+    echo "No player data available.";
+}
+?>
